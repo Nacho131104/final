3f255b208f00cddec8fa1a5100672d931ccbaaa5
Nacho <nacholopezamat@gmail.com>
2025-12-18 09:43:50 -0500

    types

---
+++ src/types.ts
@@ -0,0 +1,47 @@
+import { ObjectId } from "mongodb";
+
+
+
+export enum PokemonType {
+    NORMAL,
+    FIRE,
+    WATER,
+    ELECTRIC,
+    GRASS,
+    ICE,
+    FIGHTING,
+    POISON,
+    GROUND,
+    FLYING,
+    PSYCHIC,
+    BUG,
+    ROCK,
+    GHOST,
+    DRAGON
+}
+export type Pokemon ={
+    _id?: ObjectId,
+    name: string,
+    description: string,
+    height: number,
+    weight: number,
+    types: string[]
+}
+
+
+export type Trainer ={
+    _id: ObjectId,
+    name:string,
+    pokemons: string[]
+}
+
+export type OwnedPokemon ={
+    _id: ObjectId,
+    pokemon: string
+    nickname: string
+    attack: number
+    defense: number
+    speed: number
+    special: number
+    level: number
+}
